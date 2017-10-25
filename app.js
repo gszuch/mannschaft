1057bcdb79ab4b02c507b0195367732c81311ddf
Julian Loftis <jml87@students.uwf.edu>
2017-10-24 19:32:38 -0500

    Changed solr host ip

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const rollbar = new Rollbar("e23f0a58640f4d118026e1dddc31b822");
 // Connecting to Solr
 
 const client = new SolrNode ({
-	host: '10.0.1.4',
+	host: '52.171.129.65',
 	port: '8983',
 	core: 'testCore',
-	protocol: 'http'
+	protocol: 'https'
 });
 
 
