d91dc9487f24d9daf10ef09a93df9f0488e58d92
Julian Loftis <jml87@students.uwf.edu>
2017-11-14 17:58:57 -0600

    Shouldnt have commented out Greg's code lol

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set('view engine', 'hbs');
 
 app.set('port', process.env.PORT || 3000);
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(Express.static(Path.join(__dirname + '/public')));
 app.use('/document', Express.static(__dirname + '/public'));
 app.use(BodyParser.json());
