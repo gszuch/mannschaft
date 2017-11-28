69b86c774a4c44129c16ff510badcc794c9cabc8
Patrick Taylor <patrick@bit-wizards.com>
2017-11-28 14:40:01 -0600

    removed a few more old-school "var" type variable from app.js and reformatted app.js for best practices

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-// node modules
+const cookieSessionMath = {
+	base: 60 * 1000,
+	duration: this.base * 60,
+	activeDuration: this.base * 10
+}
+
 const Express = require('express');
 const Rollbar = require('rollbar');
 const Path = require('path');
 const BodyParser = require("body-parser");
 const Session = require("client-sessions");
-const SolrNode = require("./lib/solr-node");
 const fs = require("fs");
 const Multer = require("multer");
 const FileUpload = require("express-handlebars");
 const RandomID = require("random-id");
+const SolrNode = require("./lib/solr-node");
 
-// initialize plugins
+const uploadBranchLogic = require("./includes/uploadBranch.js");
+const login = require('./includes/login.js');
+const fileManager = require('./includes/fileManager.js');
+const mergeLogic = require("./includes/merge.js");
+const uploadLogic = require('./includes/upload.js');
+const documentLogic = require("./includes/document.js");
+const fileLogic = require("./includes/file.js");
+const createLogic = require('./includes/create.js');
+const branchLogic = require('./includes/branch.js');
+const editLogic = require('./includes/edit.js');
+
+
 const app = Express();
 const rollbar = new Rollbar("e23f0a58640f4d118026e1dddc31b822");
-
-// Connecting to Solr
+const upload = Multer({ dest: 'uploads/'});
+const Hbs = require('express-handlebars')
+.create({
+	extname: '.hbs',
+	defaultLayout: 'main',
+	partialsDir: 'views/partials/',
+	layoutsDir: 'views/layouts/',
+	helpers: require("./public/js/helpers.js").helpers
+});
 const client = new SolrNode({
     host: '127.0.0.1',
     protocol: 'http',
@@ -22,31 +45,10 @@ const client = new SolrNode({
 	port: '8983'
 });
 
-// Direct file uploads
-const upload = Multer({ dest: 'uploads/'});
-
-// define constants
-const cookieSessionMath = {
-	base: 60 * 1000,
-	duration: this.base * 60,
-	activeDuration: this.base * 10
-}
-
-// set up handlebars view engine
-const Hbs = require('express-handlebars')
-	.create({
-		extname: '.hbs',
-		defaultLayout: 'main',
-		partialsDir: 'views/partials/',
-		layoutsDir: 'views/layouts/',
-		helpers: require("./public/js/helpers.js").helpers
-	});
 
 app.engine('hbs', Hbs.engine);
 app.set('view engine', 'hbs');
 app.set('port', process.env.PORT || 3000);
-
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(Express.static(Path.join(__dirname + '/public')));
 app.use('/document', Express.static(__dirname + '/public'));
 app.use(BodyParser.json());
@@ -58,107 +60,38 @@ app.use(Session({
 	activeDuration: cookieSessionMath.activeDuration
 }));
 
-// primary views and relevant routes / aliases
-const login = require('./includes/login.js');
-app.get(['/','/login'], function(req,res){
-	login.loginGet(req,res);
-});
 
-app.post(['/','/login'], function(req, res) {
-	login.loginPost(req,res);
-});
+app.get('/branch-type', function(req,res){branchLogic.branchGet(req, res);});
+app.get('/create', function(req, res) {createLogic.createGet(req, res);});
+app.get('/edit', function(req,res){editLogic.editGet(req, res);});
+app.get('/file-manager', function(req,res){fileManager.fileManagerGet(client, req, res);});
+app.get('/file/:docID', function(req,res) {fileLogic.fileGet(client, req, res);});
+app.get('/logout', function(req, res) {req.session.reset();res.redirect('/');});
+app.get('/upload-branch', function(req, res) {uploadBranchLogic.uploadBranchGet(client, req, res);});
+app.get('/upload', function(req,res){uploadLogic.uploadGet(req, res);});
+app.get("/document/:docID", function(req,res) {documentLogic.documentGet(client, req, res);});
+app.get(['/','/login'], function(req,res){login.loginGet(req,res);});
 
-// File Manager
-const fileManager = require('./includes/fileManager.js');
-app.get('/file-manager', function(req,res){
-	fileManager.fileManagerGet(client, req, res);
-});
 
-// Searching the File Manager
-app.post('/file-manager', function(req,res) {
-	fileManager.fileManagerPost(client, req, res);
-});
-
-// Merge
-const mergeLogic = require("./includes/merge.js");
-app.post("/merge", function(req, res) {
-	mergeLogic.mergePost(client, req, res);
-});
-
-// Upload
-const uploadLogic = require('./includes/upload.js');
-app.get('/upload', function(req,res){
-	uploadLogic.uploadGet(req, res);
-});
-
-app.post('/upload', upload.single("uploadedFile"), function(req, res) {
-	uploadLogic.uploadPost(client, fs, req, res);
-});
-
-// Upload to Branch
-var uploadBranchLogic = require("./includes/uploadBranch.js")
-app.get('/upload-branch', function(req, res) {
-	uploadBranchLogic.uploadBranchGet(client, req, res);
-});
-
+app.post('/create', function(req, res) {createLogic.createPost(req, res);});
+app.post('/file-manager', function(req,res) {fileManager.fileManagerPost(client, req, res);});
+app.post('/upload', upload.single("uploadedFile"), function(req, res) {uploadLogic.uploadPost(client, fs, req, res);});
+app.post("/merge", function(req, res) {mergeLogic.mergePost(client, req, res);});
 app.post("/upload-branch", upload.single("uploadedFile"), function(req, res) {
 	//console.log("File before logic call: " + req.file);
 	uploadBranchLogic.uploadBranchPost(client, fs, req, res);
 });
+app.post(['/','/login'], function(req, res) {login.loginPost(req,res);});
 
-// Document
-var documentLogic = require("./includes/document.js");
-app.get("/document/:docID", function(req,res) {
-	documentLogic.documentGet(client, req, res);
-});
 
-// File Download
-var fileLogic = require("./includes/file.js");
-app.get('/file/:docID', function(req,res) {
-	fileLogic.fileGet(client, req, res);
-});
-
-// Create Text File for Solr Entry
-var createLogic = require('./includes/create.js');
-app.get('/create', function(req, res) {
-	createLogic.createGet(req, res);
-});
-
-app.post('/create', function(req, res) {
-	createLogic.createPost(req, res);
-});
-
-// Branch Type - DONT REMOVE
-var branchLogic = require('./includes/branch.js');
-app.get('/branch-type', function(req,res){
-	branchLogic.branchGet(req, res);
-});
-
-// Edit
-var editLogic = require('./includes/edit.js');
-app.get('/edit', function(req,res){
-	editLogic.editGet(req, res);
-});
-
-// Logout
-app.get('/logout', function(req, res) {
-	req.session.reset();
-	res.redirect('/');
-});
-
-// 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
-});
-
-// 500 error handler (middleware)
+app.use(function(req, res, next){res.status(404);res.render('404');});
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 	rollbar.log('err.stack: ' + err.stack);
 });
+
 
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:' +
